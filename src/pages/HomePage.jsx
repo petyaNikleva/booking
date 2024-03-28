8c9599bc515ccd0210a7f9c6f5648ed2fa3f72cb
P N <petyanikleva@yahoo.com>
2024-03-28 14:08:47 +0200

    add Listing filter and search functionality

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,48 @@
-import { listings } from '@/api/data/listings';
-import ListingList from '@/components/ListingList/ListingList.jsx';
+import { useState } from 'react';
+
+import {
+  isListingAvailable,
+  listings as staticListings,
+} from '@/api/data/listings';
+import ListingFilters from '@/components/ListingFilters';
+import ListingList from '@/components/ListingList';
+import { Separator } from '@/components/ui';
 
 const HomePage = () => {
+  const [listings, setListings] = useState(staticListings);
+
+  const handleFilters = (filters) => {
+    const { dates, guests, search } = filters;
+
+    let filteredListings = staticListings;
+
+    if (dates) {
+      filteredListings = filteredListings.filter((listing) =>
+        isListingAvailable(listing, dates),
+      );
+    }
+
+    if (guests) {
+      filteredListings = filteredListings.filter(
+        (listing) => guests <= listing.maxGuests,
+      );
+    }
+
+    if (search) {
+      filteredListings = filteredListings.filter((listing) =>
+        listing.name.toLowerCase().includes(search.toLowerCase()),
+      );
+    }
+
+    setListings(filteredListings);
+  };
+
   return (
     <div className='container py-4'>
+      <div className='mb-4'>
+        <ListingFilters onChange={handleFilters} />
+        <Separator className='my-4' />
+      </div>
       <ListingList listings={listings} />
     </div>
   );
