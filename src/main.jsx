ebfbfc0b442898f7cd7243b732e493a399e3cc6d
P N <petyanikleva@yahoo.com>
2024-04-10 12:45:43 +0300

    add authentication, accessToken and refreshToken

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import { seedLocalDatabase } from '@/api/data/seed';
+import AuthProvider from '@/components/AuthProvider';
 import ThemeProvider from '@/components/ThemeProvider';
+import { store } from '@/state/store';
 
-import Router from './Router.jsx';
-import { store } from './state/store.js';
+import Router from './Router';
 
 import './index.css';
 
@@ -15,7 +16,9 @@ seedLocalDatabase();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider>
     <Provider store={store}>
-      <Router />
+      <AuthProvider>
+        <Router />
+      </AuthProvider>
     </Provider>
   </ThemeProvider>,
 );
